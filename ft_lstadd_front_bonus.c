765a15a115c37b8439b74243e5460606fc2b16e5
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:41:24 -0300

    refactor: improve readability

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:35:40 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 19:20:37 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:44:28 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_lstadd_front(t_list **list, t_list *new)
 {
 	if (!list || !new)
 		return ;
-	new -> next = *list;
+	new->next = *list;
 	*list = new;
 }
