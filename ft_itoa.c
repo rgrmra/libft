c3f4cb25b3b28258af2e8e53cda40c5b3b17e5d7
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:33:07 -0300

    refactor: change ft_calloc to malloc and improve readability

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,20 +6,21 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 21:03:57 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 20:24:34 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 13:32:51 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 static int	count_digits(long nbr)
 {
 	int	digits;
 
-	if (nbr == 0)
+	if (!nbr)
 		return (1);
 	digits = 0;
-	while (nbr > 0)
+	while (nbr)
 	{
 		nbr /= 10;
 		digits++;
@@ -29,12 +30,10 @@ static int	count_digits(long nbr)
 
 static int	check_sign(long *nbr)
 {
-	if (*nbr < 0)
-	{
-		*nbr = *nbr * -1;
-		return (1);
-	}
-	return (0);
+	if (*nbr >= 0)
+		return (0);
+	*nbr = *nbr * -1;
+	return (1);
 }
 
 char	*ft_itoa(int n)
@@ -42,23 +41,23 @@ char	*ft_itoa(int n)
 	int		size;
 	int		sign;
 	long	nbr;
-	char	*new;
+	char	*p;
 
 	nbr = (long) n;
 	sign = check_sign(&nbr);
 	size = count_digits(nbr);
-	new = (char *) ft_calloc(sign + size + 1, sizeof(char));
-	if (!new)
-		return (0);
-	if (nbr == 0)
-		*(new) = '0';
-	*(new + sign + size--) = '\0';
-	while (nbr > 0)
+	p = (char *) malloc(sizeof(char) * (sign + size + 1));
+	if (!p)
+		return (NULL);
+	if (!nbr)
+		*(p) = '0';
+	*(p + sign + size--) = '\0';
+	while (nbr)
 	{
-		*(new + size-- + sign) = (nbr % 10) + 48;
+		*(p + size-- + sign) = (nbr % 10) + '0';
 		nbr /= 10;
 	}
 	if (sign)
-		*(new) = '-';
-	return (new);
+		*(p) = '-';
+	return (p);
 }
