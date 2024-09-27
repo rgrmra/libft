985143ebffbdcf9e8b5f593eff78206fbc178847
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:43:01 -0300

    refactor: change headers and improve readability

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,16 +6,17 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:36:53 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 19:21:21 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:44:49 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstdelone(t_list *list, void (*del)(void *))
 {
 	if (!list || !del)
 		return ;
-	del(list -> content);
+	del(list->content);
 	free(list);
 }
