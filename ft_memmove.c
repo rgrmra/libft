c78d207a1d249e1ce627271ddc8c5fe6ddc459f7
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:51:04 -0300

    refactor: change logic and improve readability

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,25 +6,27 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 19:39:15 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/31 18:54:55 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:45:39 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*s;
-	unsigned char	*d;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
-	if (src == dest || !n)
+	if (!n || src == dest)
 		return (dest);
-	s = (unsigned char *) src;
-	d = (unsigned char *) dest;
-	if (dest < src)
+	p1 = (unsigned char *) dest;
+	p2 = (unsigned char *) src;
+	if (dest > src)
 		ft_memcpy(dest, src, n);
 	else
 		while (n--)
-			*(d + n) = *(s + n);
+			*p1++ = *p2++;
 	return (dest);
 }
