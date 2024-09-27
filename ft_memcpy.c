bb73b201c4b8b6eb0471c0da2465aabeac5d0b43
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:49:04 -0300

    fix: correct the logic of memcpy refactor: change headers and improve readability

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,25 +6,22 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 17:41:54 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/15 13:17:44 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:44:43 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*s;
-	unsigned char	*d;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
 	if (!dest && !src)
-		return (0);
-	s = (unsigned char *) src;
-	d = (unsigned char *) dest;
-	if (dest < src)
-		while (n--)
-			*d++ = *s++;
-	else
-		ft_memmove(dest, src, n);
+		return (NULL);
+	p1 = (unsigned char *) dest;
+	p2 = (unsigned char *) src;
+	while (n--)
+		*(p1 + n) = *(p2 + n);
 	return (dest);
 }
