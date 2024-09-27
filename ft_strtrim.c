77117e2179c6bad9d34182a2bca98266ee0bdd46
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 14:04:16 -0300

    refactor: change headers and improve readability

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,19 +6,24 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:56:49 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 20:21:42 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 13:06:05 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*new;
+	char	*p;
 	size_t	size;
 
 	if (!s1 || !set)
-		return (0);
+		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size = ft_strlen(s1);
@@ -26,6 +31,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(""));
 	while (&(*(s1 + size)) && ft_strchr(set, *(s1 + size)))
 		size--;
-	new = ft_substr(s1, 0, ++size);
-	return (new);
+	p = ft_substr(s1, 0, ++size);
+	return (p);
 }
