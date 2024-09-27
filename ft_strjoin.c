93e907f6a2c00d0d3abb4cf83a4277e43f4e559e
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:58:35 -0300

    refactor: change headers and ft_calloc to malloc and improve readability

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,26 +6,31 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 21:17:55 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 20:18:07 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:14:33 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*new;
+	char	*p;
 	size_t	size1;
 	size_t	size2;
 
 	if (!s1 || !s2)
-		return (0);
+		return (NULL);
 	size1 = ft_strlen(s1);
 	size2 = ft_strlen(s2);
-	new = (char *) ft_calloc(size1 + size2 + 1, sizeof(char));
-	if (!new)
-		return (0);
-	ft_strlcpy(new, s1, size1 + 1);
-	ft_strlcat(new, s2, size1 + size2 + 1);
-	return (new);
+	p = (char *) malloc(sizeof(char) * (size1 + size2 + 1));
+	if (!p)
+		return (NULL);
+	ft_strlcpy(p, s1, size1 + 1);
+	ft_strlcat(p, s2, size1 + size2 + 1);
+	return (p);
 }
