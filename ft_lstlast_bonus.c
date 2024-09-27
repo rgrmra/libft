7c9533ee6925a3372eaca456a50202ca756eabf4
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:44:14 -0300

    refactor: improve readability

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:20:13 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 19:22:51 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:45:28 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 t_list	*ft_lstlast(t_list *list)
 {
 	if (!list)
-		return (0);
-	while (list -> next != 0)
-	{
-		list = list -> next;
-	}
+		return (NULL);
+	while (list->next)
+		list = list->next;
 	return (list);
 }
