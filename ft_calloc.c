6df60ed96571b4a58072375cc173b91dcc5622be
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:27:22 -0300

    refactor: change headers and improve readability

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,21 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:04:22 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/14 13:49:36 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:32:41 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+void	*ft_bzero(void *s, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*alloc;
 	size_t	total_size;
 
-	alloc = 0;
+	alloc = NULL;
 	total_size = nmemb * size;
 	if (!nmemb || !size || nmemb == total_size / size)
 		alloc = (void *) malloc(total_size);
