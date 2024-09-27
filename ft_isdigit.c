f9965bcf89a869e773503a8e8aa190c607c7e685
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:31:32 -0300

    refactor: invert logic to improve readability

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 21:04:32 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/05 16:39:06 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:35:22 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	if (c < '0' || c > '9')
+		return (0);
+	return (1);
 }
