3c62b16ae6d255e4f212603403d2440bb25744e8
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:53:59 -0300

    refactor: change headers and improve readability

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,11 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:21:12 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/27 19:12:15 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:00:45 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+void	ft_putchar_fd(char c, int fd);
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -23,7 +23,7 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 	}
 	if (nbr < 10)
-		ft_putchar_fd(nbr + 48, fd);
+		ft_putchar_fd(nbr + '0', fd);
 	else if (nbr >= 10)
 	{
 		ft_putnbr_fd(nbr / 10, fd);
