bb61293b9f40520f6b864f332dc6ef34f06cecb4
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 14:03:16 -0300

    refactor: change headers and improve readability

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,14 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 19:49:17 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/15 19:00:27 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:19:33 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -23,9 +26,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	while (len && *(big + i) && llen <= len--)
 	{
-		if (ft_strncmp(big + i, little, llen) == 0)
+		if (!ft_strncmp(big + i, little, llen))
 			return ((char *) big + i);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
