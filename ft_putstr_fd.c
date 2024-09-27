6804a351356604a9a517af39f776f4984237bb7a
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:54:31 -0300

    refactor: change headers

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,11 +6,14 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:12:26 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/27 19:06:27 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:01:17 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s);
 
 void	ft_putstr_fd(char *s, int fd)
 {
