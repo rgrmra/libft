855fd580824b2aacbc04696681eacbeff1335155
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:53:39 -0300

    refactor: change headers

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,11 +6,12 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:17:53 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/15 16:28:54 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:59:57 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
 
 void	ft_putendl_fd(char *s, int fd)
 {
