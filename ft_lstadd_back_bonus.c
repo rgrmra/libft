558f917df13246c74ae2df6196db559331b6ee1d
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 13:40:36 -0300

    refactor: improve readability

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,18 +6,20 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:24:32 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/10/29 19:20:07 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 11:44:01 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*ft_lstlast(t_list *lst);
+
 void	ft_lstadd_back(t_list **list, t_list *new)
 {
 	if (!list || !new)
 		return ;
-	if (!(*list))
+	if (!*list)
 		*list = new;
 	else
-		ft_lstlast(*list)-> next = new;
+		ft_lstlast(*list)->next = new;
 }
