f69f231f93a854a0dd2c684ecccb2f7229eeb401
Roger de Moura <rde-mour@student.42sp.org.br>
2024-09-27 14:02:34 -0300

    refactor: change headers and improve readability

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,24 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 16:06:12 by rde-mour          #+#    #+#             */
-/*   Updated: 2023/11/02 17:30:41 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/09/27 12:18:27 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
-	str1 = (unsigned char *) s1;
-	str2 = (unsigned char *) s2;
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
 	i = 0;
-	while (*(str1 + i) && *(str2 + i) && *(str1 + i) == *(str2 + i) && i < n)
+	while (*(p1 + i) && *(p2 + i) && *(p1 + i) == *(p2 + i) && i < n)
 		i++;
 	if (i != n)
-		return (*(str1 + i) - *(str2 + i));
+		return (*(p1 + i) - *(p2 + i));
 	return (0);
 }
